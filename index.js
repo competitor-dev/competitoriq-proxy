11eb43ebfd21db3933a543e306c33b9a3dab6611
competitor-dev <alimurungrico@gmail.com>
2026-05-22 14:34:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Proxy running on port ${PORT}`));
